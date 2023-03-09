74e659a4184c88b433b8c1382562ffbff001415c
Tue Tran <tranthanhtue@gmail.com>
2023-03-09 11:40:17 +0700

    route

--- src/index.js
+++ src/index.js
@@ -7,22 +7,23 @@ const app = express();
 
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname,"resource","public")));
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 //HTTP logger
-app.use(morgan("combined"));
+//app.use(morgan("combined"));
 
 //Template engine
 app.engine("hbs", hbs.engine({defaultLayout: 'main', extname:'.hbs'}));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname,"resource","views"));
 
-app.get("/", (req, res) => {
-  res.render('home');
-});
-app.get("/news", (req, res) => {
-  res.render('news');
-});
+//routes init
+route(app);
+
 app.listen(port, () => {
   console.log(`excample app listening at http://localhost:${port}`);
 });
